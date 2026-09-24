549aea7082adee764468e76ca9ffc59dc69facb6
lino <lino_snsalias@outlook.com>
2026-09-24 13:07:23 +0000

    Change `TrAsyncIterator` API to reduce misunderstood;

--- src/async_iter_.rs
+++ src/async_iter_.rs
@@ -6,7 +6,7 @@ pub trait TrAsyncIterator {
     type Err: core::error::Error;
 
     type NextAsync<'f>: TrMayCancel<'f, MayCancelOutput =
-        Result<Option<Self::Item>, Self::Err>>
+        Result<Self::Item, Self::Err>>
     where
         Self: 'f;
 
@@ -18,14 +18,30 @@ where
     I: core::iter::Iterator,
 {
     type Item = <Self as Iterator>::Item;
-    type Err = !;
+    type Err = IterEndedError;
 
     type NextAsync<'f> = core::future::
-        Ready<Result<Option<Self::Item>, Self::Err>>
+        Ready<Result<Self::Item, Self::Err>>
     where
         Self: 'f;
 
     fn next_async(&mut self) -> Self::NextAsync<'_> {
-        core::future::ready(Result::Ok(self.next()))
+        if let Option::Some(item) = self.next() {
+            core::future::ready(Result::Ok(item))
+        } else {
+            core::future::ready(Result::Err(IterEndedError))
+        }
     }
 }
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct IterEndedError;
+
+impl core::fmt::Display for IterEndedError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "IterEndedError")
+    }
+}
+
+impl core::error::Error for IterEndedError
+{}
